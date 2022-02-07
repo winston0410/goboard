7026f0e13fd7cb25c0899112de637a308596df2a
kghugo <hugosum.dev@protonmail.com>
2022-02-07 14:43:14 +0000

    Init

---
+++ web/flake.nix
@@ -0,0 +1,58 @@
+{
+
+  inputs = {
+    naersk.url = "github:nmattia/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk, rust-overlay }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in {
+
+        defaultPackage = naersk-lib.buildPackage ./.;
+
+        defaultApp = utils.lib.mkApp { drv = self.defaultPackage."${system}"; };
+
+        devShell = with pkgs;
+          mkShell {
+            nativeBuildInputs = [
+              pkgconfig
+              clang
+              lld # To use lld linker
+            ];
+            buildInputs = [
+              cargo
+              ((rust-bin.selectLatestNightlyWith
+                (toolchain: toolchain.default)).override {
+                  targets = [ "wasm32-unknown-unknown" ];
+                })
+              wasm-pack
+              rustfmt
+              pre-commit
+              rustPackages.clippy
+              alsa-lib
+              udev
+              #NOTE Add more deps
+              vulkan-loader
+              xorg.libX11
+              x11
+              xorg.libXrandr
+              xorg.libXcursor
+              xorg.libXi
+            ];
+            shellHook = ''
+              export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
+                pkgs.lib.makeLibraryPath [ udev alsaLib vulkan-loader ]
+              }"'';
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+
+      });
+
+}
